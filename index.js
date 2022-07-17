c1b4e2b30ba837dc3043c5731129d93e81898e83
Niilo Lappi <thenipanen@DESKTOP-S5RVM6C.localdomain>
2022-07-17 20:52:26 +0300

    WIP

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 morgan.token('body', function (req, res) { return req.method === 'POST' ? JSON.stringify(req.body) : '' })
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -80,7 +82,7 @@ app.get('/info', (request, response) => {
   response.send(`<div>Phonebook has info for ${persons.length} people</div><div>${new Date()}</div>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
