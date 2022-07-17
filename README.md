58e422d146b63a4c276c5ed9f3a7e6c629609b9c
Niilo Lappi <thenipanen@DESKTOP-S5RVM6C.localdomain>
2022-07-17 21:18:31 +0300

    Whole app in internet

---
+++ README.md
@@ -0,0 +1,1 @@
+https://boiling-coast-64070.herokuapp.com
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 morgan.token('body', function (req, res) { return req.method === 'POST' ? JSON.stringify(req.body) : '' })
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
